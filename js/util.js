95af0d3468215c3ecc44182c796637e1d4863b5f
JingxuanWang <wjx1985@gmail.com>
2013-06-21 20:12:47 +0900

    update

--- js/util.js
+++ js/util.js
@@ -1,27 +1,66 @@
 // bind a function with a specific scope
 function bind(func, scope){
-    return function(){
-        return func.apply(scope, arguments);
-    };
-}       
-
-// sort an object array by it's property
-function sortBy(prop, order) {
-    return function(a, b) {
-        return order * (a[prop] - b[prop]);
-    };
-}   
+	return function(){
+		return func.apply(scope, arguments);
+	};
+}	   
 
 // deep clone a object
 function clone(obj){
-    if(obj == null || typeof(obj) != 'object')
-        return obj;
+	if(obj == null || typeof(obj) != 'object')
+		return obj;
 
-    var temp = {};
-    //var temp = obj.constructor(); // changed
+	var temp = {};
+	//var temp = obj.constructor(); // changed
 
-    for(var key in obj)
-        temp[key] = clone(obj[key]);
-    return temp;
+	for(var key in obj)
+		temp[key] = clone(obj[key]);
+	return temp;
 }
 
+// sum up some prop in an array of objects
+function sum(arr, prop) {
+	return arr.map(function(k) {
+		return k[prop];
+	}).reduce(function(a, b) {
+		return a + b;
+	});
+}
+
+// sum up some prop in an array of objects
+function sum2(arr, func) {
+	return arr.map(function(k) {
+		return func(k);
+	}).reduce(function(a, b) {
+		return a + b;
+	});
+}
+
+// sort an object array by it's property
+function sortByProp(arr, prop, order) {
+	return arr.sort(function(a, b) {
+		return order * (a[prop] - b[prop]);
+	});
+}
+
+//Schwartzian transform
+function sortByFunc(arr, func, order) {
+	return arr.map(function (x) {
+		return [x, func(x)];
+	}).sort(function (a, b) {
+		return order * (a[1] - b[1]);
+	}).map(function (x) {
+		return x[0];
+	});
+}
+
+function lot(arr, func, total_prob) {
+	total_prob ||= sum2(arr, func);
+	var rand = Math.floor((Math.random() * total_prob) + 1);  // 1 ~ total_prob
+	for (var i = 0; i < arr.length; i++) {
+		rand -= func(arr);
+		if (rand < 0) {
+			return arr[i];
+		}	
+	}
+}
